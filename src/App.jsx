f0534256c76d0fb32dcc191b08f982353b6891c6
JmGarcia30 <jmjgarcia.student@ua.edu.ph>
2025-09-18 23:57:03 +0800

    Updated Code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div class="container">
-
-      <div class="section">
-        <div class="box"></div>
-        <div class="peach-box">
-          <div class="red-group">
-            <div class="inner"></div>
-            <div class="inner"></div>
-          </div>
-        </div>
-        <div class="box"></div>
-      </div>
-
-      <div class="middle">
-        <div class="left"></div>
-        <div class="right">
-          <div></div>
-          <div></div>
-        </div>
-      </div>
-
-      <div class="bottom">
-        <div class="side"></div>
-        <div class="center">
-          <div></div>
-          <div></div>
-        </div>
-        <div class="side"></div>
-      </div>
-
-    </div>
-  )
+function Box(props) {
+  return <div className="box">{props.label}</div>;
 }
 
-export default App
+function InnerBox(props) {
+  return <div className="inner-box">{props.title}</div>;
+}
+
+function PeachGroup() {
+  return (
+    <div className="peach-group">
+      <InnerBox title="JM" />
+      <InnerBox title="Garcia" />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <div className="header">
+      <Box />
+      <PeachGroup />
+      <Box />
+    </div>
+  );
+}
+
+function LeftPane(props) {
+  return <div className="left-pane">{props.label}</div>;
+}
+
+function RightItem(props) {
+  return <div className="right-item">{props.label}</div>;
+}
+
+function RightPane() {
+  return (
+    <div className="right-pane">
+      <RightItem />
+      <RightItem />
+    </div>
+  );
+}
+
+function Middle() {
+  return (
+    <div className="middle">
+      <LeftPane />
+      <RightPane />
+    </div>
+  );
+}
+
+function Side(props) {
+  return <div className="side">{props.label}</div>;
+}
+
+function CenterItem(props) {
+  return <div className="center-item">{props.label}</div>;
+}
+
+function CenterGroup() {
+  return (
+    <div className="center-group">
+      <CenterItem label="C-PCIT9" />
+      <CenterItem label="IT3B" />
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <Side />
+      <CenterGroup />
+      <Side />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Middle />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
